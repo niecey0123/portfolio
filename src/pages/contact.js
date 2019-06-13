189223a9858862271ab949d7144d8ffa793883a0
shaniece crumpler <s.crumpler11@gmail.com>
2019-06-13 10:28:08 -0400

    trying to get form submission to work

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ export default () => (
         <div>
     <h1>I'd love to talk! Email me at the address below</h1>
 
-    {/* <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
       
         <div className="field">
         <label htmlFor="name" className="label">Name</label>
@@ -58,29 +58,8 @@ export default () => (
 
 
    
-      </form> */}
+      </form>
 
-
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
         </div>
         </Layout>
 
