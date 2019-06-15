3a5bd84f5c7bff8ae169ea332e873bfa5e23f0b3
shaniece crumpler <s.crumpler11@gmail.com>
2019-06-15 17:10:14 -0400

    making changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,73 +5,11 @@ import DummyForm from "../components/dummyForm"
 
 
 export default () => (
-    <Layout>
+ <Layout>
 
 <DummyForm/>
+</Layout>
 
-        {/* <div>
-    <h1>I'd love to talk! Email me at the address below</h1>
-
-    <form name="contact" action="POST" data-netlify="true" >
-      
-        <div className="field">
-        <label htmlFor="name" className="label">Name</label>
-        <div className="control">
-            <input  className="input" name="name" type="text" placeholder="name" />
-        </div>
-        </div>
-
-
-        <div className="field">
-        <label htmlFor="email" className="label">Email</label>
-        <div className="control has-icons-left has-icons-right">
-            <input  name="email" className="input is-info" type="email" placeholder="Email"  />
-            <span className="icon is-small is-left">
-            <i className="fas fa-envelope" />
-            </span>
-            <span className="icon is-small is-right">
-            <i className="fas fa-exclamation-triangle" />
-            </span>
-        </div>
-        </div>
-
-
-        <div className="field">
-            <label htmlFor="subject" className="label">Subject</label>
-            <div className="control">
-                <div className="select">
-                <select name= "inquiry">
-                    <option >Select dropdown</option>
-                    <option >Job Opportunity</option>
-                </select>
-                </div>
-            </div>
-            </div>
-
-            <div className="field">
-            <label htmlFor="message" className="label">Message</label>
-            <div className="control">
-                <textarea className="textarea" name="message" placeholder="Textarea"></textarea>
-            </div>
-            </div>
-
-            {/* <div className="field">
-            <div data-netlify-recaptcha="true">
-            </div>
-            </div> */}
-
-         <div className="field is-grouped">
-        <div className="control">
-            <button type="submit" className="button is-link">Submit</button>
-        </div>
-        </div>
-
-
-   
-      </form>
-
-        </div> */}
-        </Layout>
 
   
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/layout"
-import DummyForm from "../components/dummyForm"
 
 
 export default () => (
